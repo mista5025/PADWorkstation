1b64b2138bfb48f3c165bcd4544b67ea0fc1a16d
Lawrence <41893295+mista5025@users.noreply.github.com>
2019-02-27 15:44:31 +1300

    Finished Front End

---
+++ IndustryProject/RplProgramDocument List/Controllers/CreateNewController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RplProgramDocument_List.Controllers
+{
+    public class CreateNewController : Controller
+    {
+
+        public IActionResult SectionInfo()
+        {
+            return View();
+        }
+        public IActionResult CreateSectionOne()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSectionTwo()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSectionThree()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSectionFour()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSectionFive()
+        {
+            return View();
+        }
+    }
+}
---
+++ IndustryProject/RplProgramDocument List/Controllers/DashboardController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RplProgramDocument_List.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
